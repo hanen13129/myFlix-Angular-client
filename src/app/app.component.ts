53cd3fb429d9a78e91671509c4fd419a925ce120
hanen Ben hassen <79134588+hanen13129@users.noreply.github.com>
2021-12-31 22:23:14 +0100

    6.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) {}
-
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px',
-    });
-  }
-
-  openLoginDialog(): void {
-    this.dialog.open(LoginFormComponent, {
-      width: '280px',
-    });
-  }
 }
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -2,23 +2,24 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
+
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
   @Input() userCredentials = { Username: '', Password: '' };
-
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<LoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+     public router: Router
   ) {}
 
-  ngOnInit(): void {}
-
+  ngOnInit(): void {} 
   login(): void {
     this.fetchApiData.userLogin(this.userCredentials).subscribe(
       (result) => {
@@ -26,16 +27,27 @@ export class LoginFormComponent implements OnInit {
 
         localStorage.setItem('token', result.token);
         localStorage.setItem('user', JSON.stringify(result.user));
-        console.log(result);
+        //console.log(result);
         this.snackBar.open(result, 'OK', {
           duration: 2000,
         });
+        this.router.navigate(['movies']);
       },
       (result) => {
-        console.log(result);
+        //console.log(result);
         this.snackBar.open(result, 'OK', {
           duration: 2000,
         });
+
+    
+          
+            
+    
+
+          
+    
+    
+  
       }
     );
   }
